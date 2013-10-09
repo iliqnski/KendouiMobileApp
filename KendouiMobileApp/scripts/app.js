88c1d40c6fe0510040f8624d4c4daa71f1be391a
Iliyan Hristov <iliqn408@abv.bg>
2013-10-09 09:56:07 -0500

    Changes made by Iliyan Hristov (iliqn408@abv.bg).

--- KendouiMobileApp/scripts/app.js
+++ KendouiMobileApp/scripts/app.js
@@ -1,21 +1,23 @@
 (function (global) {
-    var mobileSkin = "",
-        app = global.app = global.app || {};
-
+  
+    window.cars = [
+        {Vendor: "Audi", Model: "A6", RentPrice: "2000", IsFree:"True"},
+        {Vendor: "VW", Model: "Golf", RentPrice: "1000", IsFree:"True"},
+        {Vendor: "Mercedes", Model: "E270", RentPrice: "2000", IsFree:"True"},
+        {Vendor: "BMW", Model: "320", RentPrice: "1500", IsFree:"True"},
+        {Vendor: "BMW", Model: "330", RentPrice: "2000", IsFree:"True"},
+        {Vendor: "Opel", Model: "Astra", RentPrice: "100", IsFree:"True"},
+        {Vendor: "Seat", Model: "Ibiza", RentPrice: "120", IsFree:"True"},
+    ];
+     
+    var app = global.app = global.app || {};
+  
     document.addEventListener("deviceready", function () {
-        app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
+        app.application = new kendo.mobile.Application(document.body);
+        
+        var template = kendo.template($('#carTmpl').html());
+        var processed = template(cars);
+        $('#all-cars').append(processed);
     }, false);
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
+    
 })(window);
